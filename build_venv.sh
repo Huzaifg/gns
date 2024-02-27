e8dffdfe82f477177bcc56c3f938387c6e461480
Sikan Li <teresakkan@gmail.com>
2024-02-27 12:46:19 -0600

    multinode version

--- build_venv.sh
+++ build_venv.sh
@@ -11,7 +11,7 @@ source venv/bin/activate
 
 which python
 python -m pip install --upgrade pip
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install torch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 --index-url https://download.pytorch.org/whl/cu118
 pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.0+cu118.html
 python -m pip install -r requirements.txt
 
